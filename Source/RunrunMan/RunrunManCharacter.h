aef76f220f813d9a9851925d07122e4b3a4c556c
bikekun <potoshop1@gmail.com>
2016-06-21 22:34:39 +0300

    добавил Engine.h в RunrunmanCharacter.h

--- Source/RunrunMan/RunrunManCharacter.h
+++ Source/RunrunMan/RunrunManCharacter.h
@@ -1,7 +1,9 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 #pragma once
 #include "GameFramework/Character.h"
+#include "Engine.h"
 #include "RunrunManCharacter.generated.h"
+
 
 UCLASS(config=Game)
 class ARunrunManCharacter : public ACharacter
