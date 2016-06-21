d69c6a34da68b023e27d420787d1e26c91982c14
bikekun <potoshop1@gmail.com>
2016-06-21 22:22:14 +0300

    1

---
+++ Source/RunrunMan/RunrunMan.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __RUNRUNMAN_H__
+#define __RUNRUNMAN_H__
+
+#include "EngineMinimal.h"
+				   
+#endif
---
+++ Source/RunrunMan/RunrunManGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "RunrunManGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARunrunManGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ARunrunManGameMode();
+};
+
+
+
