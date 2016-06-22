576fefd3d384be524011ac5c8dfdcce0b459eb75
bikekun <potoshop1@gmail.com>
2016-06-22 21:29:39 +0300

    Добавил AParam_RunrunManCharacter

---
+++ Source/RunrunMan/Pawn/Param_RunrunManCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "RunrunManCharacter.h"
+#include "Param_RunrunManCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUNRUNMAN_API AParam_RunrunManCharacter : public ARunrunManCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
